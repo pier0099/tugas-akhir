488e74cdad28e3be4c49b960b69c2c843678a808
pier0099 <spiderwicks123@gmail.com>
2024-06-25 20:39:11 +0700

    Step 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    strictPort: true // Ensures the server runs on the specified port without random port selection
   }
 })

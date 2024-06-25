72788e0b025fc876353e1aebc0ea8150c2b76927
pier0099 <spiderwicks123@gmail.com>
2024-06-25 19:50:51 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">Project XC</h1>
+    <h3>Inisiasi Project</h3>
+  </div>
+</template>
